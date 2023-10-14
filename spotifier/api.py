0b7c334e83ff812b7773c4f24faa8b711c96780b
navid <navid.falah7@gmail.com>
2023-10-14 12:08:34 +0330

    api divided to four

--- spotifier/api.py
+++ spotifier/api.py
@@ -16,17 +16,46 @@ def get_json_data():
     return array_code_artists
 
 
-class TopTracksOfArtist(ListAPIView):
+class TopTracksOfArtist_first(ListAPIView):
     serializer_class = TrackSerializer
 
     def get_queryset(self):
         array_artists = get_json_data()
         access_token = get_access_token(client_id=client_id, client_secret=client_secret)
-        for artist in array_artists:
+        for artist in array_artists[0:25]:
             tracks = get_top_tracks_of_artist(access_token, artist)
         return tracks
 
+class TopTracksOfArtist_second(ListAPIView):
+    serializer_class = TrackSerializer
 
+    def get_queryset(self):
+        array_artists = get_json_data()
+        access_token = get_access_token(client_id=client_id, client_secret=client_secret)
+        for artist in array_artists[25:50]:
+            tracks = get_top_tracks_of_artist(access_token, artist)
+        return tracks
+
+class TopTracksOfArtist_third(ListAPIView):
+    serializer_class = TrackSerializer
+
+    def get_queryset(self):
+        array_artists = get_json_data()
+        access_token = get_access_token(client_id=client_id, client_secret=client_secret)
+        for artist in array_artists[50:75]:
+            tracks = get_top_tracks_of_artist(access_token, artist)
+        return tracks
+
+class TopTracksOfArtist_forth(ListAPIView):
+    serializer_class = TrackSerializer
+
+    def get_queryset(self):
+        array_artists = get_json_data()
+        access_token = get_access_token(client_id=client_id, client_secret=client_secret)
+        for artist in array_artists[75:100]:
+            tracks = get_top_tracks_of_artist(access_token, artist)
+        return tracks
+    
 class DownloaderSong(ListAPIView):
     serializer_class = TrackSerializer
         
